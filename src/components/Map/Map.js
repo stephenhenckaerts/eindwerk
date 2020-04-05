e24bd51ae83c9c98e9ec106ae47b59487754036b
stephenhenckaerts <stephen.henckaerts@student.pxl.be>
2020-04-05 15:47:40 +0200

    Code Cleanup

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -7,11 +7,102 @@ import BingMaps from "ol/source/BingMaps";
 import VectorSource from "ol/source/Vector";
 import { bbox as bboxStrategy } from "ol/loadingstrategy";
 import GeoJSON from "ol/format/GeoJSON";
-import { Vector, Group } from "ol/layer";
+import { Vector, Group, Tile } from "ol/layer";
+import Select from "ol/interaction/Select";
 
 class OlMap {
   constructor() {
     this.createNewMap();
+    this.createBackgroundLayerGroups();
+  }
+
+  createBackgroundLayerGroups() {
+    this.layersOSM = new Group({
+      layers: [
+        new Tile({
+          source: new OSM()
+        })
+      ]
+    });
+    this.layersBM = new Group({
+      layers: [
+        new Tile({
+          source: new BingMaps({
+            imagerySet: "Aerial",
+            key:
+              "AsVf4lj-tiANM5N4_P56DC_oQQM9fjb0lMosBxFtgovzGEgcMnQuqYpeKpX-1KL2"
+          })
+        })
+      ]
+    });
+  }
+
+  addBoundriesLayer(featureSelected) {
+    let boundriesLayer = null;
+    this.map.getLayers().forEach(layer => {
+      if (layer.get("name") == "plotBoundriesLayer") {
+        boundriesLayer = layer;
+      }
+    });
+    if (boundriesLayer == null) {
+      let vectorSource = new VectorSource({
+        format: new GeoJSON(),
+        minScale: 15000000,
+        loader: function(extent, resolution, projection) {
+          /*
+          Link for the DLV
+          var url =
+            "http://localhost:3030/map/https://geoservices.landbouwvlaanderen.be/PUBLIC/wfs?service=WFS&request=GetFeature&version=1.1.0&typename=PUBLIC:LBGEBRPERC2019&srsname=EPSG:3857&outputFormat=application/json&count=1000&bbox=" +
+            extent.join(",") +
+            ",EPSG:3857";
+          */ var url =
+            "http://localhost:3030/maps/map/http://localhost:8080/geoserver/lbgbrprc18/wfs?service=WFS&request=GetFeature&version=1.1.0&typename=PUBLIC:Lbgbrprc18&srsname=EPSG:3857&outputFormat=application/json&count=1000&bbox=" +
+            extent.join(",") +
+            ",EPSG:3857";
+          // */
+          var xhr = new XMLHttpRequest();
+          xhr.open("GET", url);
+          var onError = function() {
+            vectorSource.removeLoadedExtent(extent);
+          };
+          xhr.onerror = onError;
+          xhr.onload = function() {
+            if (xhr.status === 200) {
+              var features = vectorSource
+                .getFormat()
+                .readFeatures(xhr.responseText);
+              features.forEach(function(feature) {
+                //ID for the DLV
+                //feature.setId(feature.get("OBJ_ID"));
+                feature.setId(feature.get("OIDN"));
+              });
+              vectorSource.addFeatures(features);
+            } else {
+              onError();
+            }
+          };
+          xhr.send();
+        },
+        strategy: bboxStrategy
+      });
+      var vector = new Vector({
+        //minZoom: 13,
+        source: vectorSource
+      });
+      this.select = new Select({
+        layers: [vector]
+      });
+      this.select.on("select", event => featureSelected(event, this.select));
+      this.map.addInteraction(this.select);
+      vector.set("name", "plotBoundriesLayer");
+      this.map.addLayer(vector);
+    } else {
+      this.select = new Select({
+        layers: [boundriesLayer]
+      });
+      this.select.on("select", event => featureSelected(event, this.select));
+      this.map.addInteraction(this.select);
+    }
   }
 
   createNewMap() {
@@ -31,87 +122,34 @@ class OlMap {
   }
 
   removeAllLayers() {
-    this.map.setLayerGroup(new Group());
+    this.map.getLayers().forEach(layer => {
+      this.map.removeLayer(layer);
+    });
+  }
+
+  clearSelect() {
+    this.select.getFeatures().clear();
   }
 
   setBackgroundTileLayer(type) {
-    let raster = null;
-    switch (type) {
-      case "OPENSTREETMAP": {
-        raster = new TileLayer({
-          source: new OSM({})
-        });
-        break;
-      }
-      case "BING MAPS": {
-        raster = new TileLayer({
-          source: new BingMaps({
-            imagerySet: "Aerial",
-            key:
-              "AsVf4lj-tiANM5N4_P56DC_oQQM9fjb0lMosBxFtgovzGEgcMnQuqYpeKpX-1KL2"
-          })
-        });
-        break;
-      }
-      default: {
-        raster = new TileLayer({
-          source: new OSM({})
-        });
-        break;
-      }
+    if (type == "OPENSTREETMAP") {
+      this.map.setLayerGroup(this.layersOSM);
+    } else if (type == "BING MAPS") {
+      this.map.setLayerGroup(this.layersBM);
     }
-    this.map.addLayer(raster);
   }
 
   togglePlotBoundriesLayers(state) {
-    let vectorSource = null;
+    let enable = false;
     if (state) {
-      vectorSource = new VectorSource({
-        format: new GeoJSON(),
-        minScale: 15000000,
-        loader: function(extent, resolution, projection) {
-          /*
-          Link for the DLV
-          let url =
-            "http://localhost:3030/map/https://geoservices.landbouwvlaanderen.be/PUBLIC/wfs?service=WFS&request=GetFeature&version=1.1.0&typename=PUBLIC:LBGEBRPERC2019&srsname=EPSG:3857&outputFormat=application/json&count=1000&bbox=" +
-            extent.join(",") +
-            ",EPSG:3857";
-          */ let url =
-            "http://localhost:3030/maps/map/http://localhost:8080/geoserver/lbgbrprc18/wfs?service=WFS&request=GetFeature&version=1.1.0&typename=PUBLIC:Lbgbrprc18&srsname=EPSG:3857&outputFormat=application/json&count=1000&bbox=" +
-            extent.join(",") +
-            ",EPSG:3857";
-          // */
-          let xhr = new XMLHttpRequest();
-          xhr.open("GET", url);
-          let onError = function() {
-            vectorSource.removeLoadedExtent(extent);
-          };
-          xhr.onerror = onError;
-          xhr.onload = function() {
-            if (xhr.status === 200) {
-              let features = vectorSource
-                .getFormat()
-                .readFeatures(xhr.responseText);
-              features.forEach(function(feature) {
-                //ID for the DLV
-                //feature.setId(feature.get("OBJ_ID"));
-                feature.setId(feature.get("OIDN"));
-              });
-              vectorSource.addFeatures(features);
-            } else {
-              onError();
-            }
-          };
-          xhr.send();
-        },
-        strategy: bboxStrategy
-      });
-      let vector = new Vector({
-        //minZoom: 13,
-        source: vectorSource
-      });
-      this.map.addLayer(vector);
+      enable = true;
     }
+    this.map.getLayers().forEach(layer => {
+      if (layer.get("name") == "plotBoundriesLayer") {
+        console.log(layer);
+        layer.setVisible(enable);
+      }
+    });
   }
 
   addTileLayer(url) {
--- src/components/Viewer/Viewer.js
+++ src/components/Viewer/Viewer.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import { connect } from "react-redux";
 
 import Map from "../Map/Map";
@@ -12,11 +12,19 @@ class Viewer extends Component {
   constructor(props) {
     super(props);
     Map.createNewMap();
+    Map.removeAllLayers();
+
+    this.popup = React.createRef();
+    this.popupContent = React.createRef();
+
+    this.mapBackgroundType = null;
   }
 
   componentDidMount() {
+    Map.setBackgroundTileLayer(this.props.type);
+    Map.addBoundriesLayer(this.featureSelected);
     Map.map.setTarget("map");
-    let container = document.getElementById("popup");
+    let container = this.popup.current;
     let overlay = new Overlay({
       element: container,
       autoPan: true,
@@ -28,7 +36,12 @@ class Viewer extends Component {
     Map.map.addOverlay(overlay);
   }
 
-  featureSelected = event => {
+  resetMapLayers() {
+    Map.setBackgroundTileLayer(this.props.type);
+    Map.togglePlotBoundriesLayers(this.props.plotBoundriesState);
+  }
+
+  featureSelected = (event, select) => {
     if (event.selected[0]) {
       this.selectedFeature = event.selected[0];
       let selectedFeature = {
@@ -41,7 +54,7 @@ class Viewer extends Component {
         coords: event.selected[0].getProperties().geometry.extent_
       };
 
-      let content = document.getElementById("popup-content");
+      let content = this.popupContent.current;
       content.innerHTML =
         "<p><strong>Gewasgroepnaam: </strong>" +
         selectedFeature.gewasgroepnaam +
@@ -58,35 +71,23 @@ class Viewer extends Component {
 
   closePopup() {
     this.overlay.setPosition(undefined);
-    this.select.getFeatures().clear();
+    Map.clearSelect();
     return false;
   }
 
   addFeature() {
     this.overlay.setPosition(undefined);
-    this.select.getFeatures().clear();
+    Map.clearSelect();
     this.props.featureAddedHandler(this.selectedFeature);
   }
 
   render() {
-    console.log(this.props.loadedPlots);
-    Map.removeAllLayers();
-    Map.setBackgroundTileLayer(this.props.type);
-    if (this.props.plotBoundriesState) {
-      Map.togglePlotBoundriesLayers(this.props.plotBoundriesState);
-      if (this.select == null) {
-        this.select = new Select({});
-        this.select.on("select", event => this.featureSelected(event));
-        Map.map.addInteraction(this.select);
-      }
-    } else {
-      Map.map.removeInteraction(this.select);
-      this.select = null;
-    }
+    //console.log(this.props.loadedPlots);
+    this.resetMapLayers();
     return (
       <div>
         <div id="map" className={styles.Map}></div>
-        <div id="popup" className={styles.OlPopup}>
+        <div ref={this.popup} className={styles.OlPopup}>
           <div className={styles.OlPopupButtonsDiv}>
             <Button
               btnType="Danger"
@@ -105,7 +106,7 @@ class Viewer extends Component {
               Voeg Toe
             </Button>
           </div>
-          <div id="popup-content"></div>
+          <div ref={this.popupContent}></div>
         </div>
       </div>
     );
