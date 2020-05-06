bde0ad7d8fe2f324ffd59d800691277b9c198c86
stephenhenckaerts <stephen.henckaerts@student.pxl.be>
2020-05-06 21:25:43 +0200

    Added Snackbar

--- src/components/Sidebar/CompareSidebar/LayersMenu/LayersMenu.js
+++ src/components/Sidebar/CompareSidebar/LayersMenu/LayersMenu.js
@@ -5,9 +5,17 @@ import dronebeelden from "../../../../assets/CompareSidebar/dronebeelden.png";
 import satelliet from "../../../../assets/CompareSidebar/satelliet.svg";
 import tractor from "../../../../assets/CompareSidebar/tractor.svg";
 
+import Snackbar from "../../../UI/Snackbar/Snackbar";
+
 import styles from "./LayersMenu.module.scss";
 
 const LayersMenu = () => {
+  let snackbarRef = React.createRef();
+
+  const clickHandler = () => {
+    snackbarRef.current.openSnackBar("Button Pressed...");
+  };
+
   return (
     <div className={styles.Menu}>
       <div className={styles.Layer}>
@@ -22,10 +30,11 @@ const LayersMenu = () => {
         <img src={dronebeelden} alt="Dronebeelden" />
         <p>Dronebeelden</p>
       </div>
-      <div className={styles.Layer}>
+      <div className={styles.Layer} onClick={clickHandler}>
         <img src={satelliet} alt="Sattelietbeelden" />
         <p>Sattelietbeelden</p>
       </div>
+      <Snackbar ref={snackbarRef} />
     </div>
   );
 };
---
+++ src/components/UI/Snackbar/Snackbar.js
@@ -0,0 +1,34 @@
+import React, { PureComponent } from "react";
+import Styles from "./Snackbar.module.scss";
+
+class Snackbar extends PureComponent {
+  message = "";
+
+  state = {
+    isActive: false,
+  };
+
+  openSnackBar = (message = "Something went wrong...") => {
+    this.message = message;
+    this.setState({ isActive: true }, () => {
+      setTimeout(() => {
+        this.setState({ isActive: false });
+      }, 3000);
+    });
+  };
+
+  render() {
+    const { isActive } = this.state;
+    return (
+      <div
+        className={
+          isActive ? [Styles.snackbar, Styles.show].join(" ") : Styles.snackbar
+        }
+      >
+        {this.message}
+      </div>
+    );
+  }
+}
+
+export default Snackbar;
